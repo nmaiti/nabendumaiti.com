2d9ee6ba54afeda26d8f32a758281536ae6cfc50
Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>
2023-03-19 01:56:08 +0530

    icon error fix
    Signed-off-by: Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>

--- src/components/icons/youtube.js
+++ src/components/icons/youtube.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const IconYoutube = () => (
-<svg width="24" height="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none">
-  <g stroke-width="0"/>
-  <g stroke-linecap="round" stroke-linejoin="round"/>
-  <g stroke="currentColor" stroke-width="1.125">
-    <path d="M15.938 9c0-4.219-.465-4.687-4.688-4.688h-4.5c-4.222 0-4.688.469-4.688 4.688s.465 4.687 4.688 4.688h4.5c4.222 0 4.688-.469 4.688-4.688Z"/>
-    <path stroke-linejoin="round" d="M11.063 9 7.688 6.938v4.125L11.063 9Z"/>
-  </g>
-</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-youtube" >
+    <title>Youtube</title>
+    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />
+    <path d="m9.75 15.02 5.75-3.27-5.75-3.27v6.54Z" />
+  </svg>
 
 );
 
